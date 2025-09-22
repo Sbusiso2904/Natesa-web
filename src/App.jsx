7b733aa942b4e16f51fd1e5671a66233de7aea61
Sbusiso2904 <sbusisombatha2904@gmail.com>
2025-09-22 03:05:12 +0200

    React files created

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+function App(){
+
+}
+export default App;
